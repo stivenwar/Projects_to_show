a65cdc8a4deb1964bb037b078604b28d87ab1d75
stivenwar <estebanguerra1990@gmail.com>
2025-04-13 19:21:36 +0200

    update requestHeaderParseMicroservice

--- controllers/ProjectsController.js
+++ controllers/ProjectsController.js
@@ -38,7 +38,6 @@ export class ProjectsController{
         let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
         const language = req.headers['accept-language'];
         const software = req.headers['user-agent'];
-
         res.json({ipaddress:ip, language:language,software:software})
     }
 
